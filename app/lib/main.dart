a21cade1c6d2dbcb1d5745e0eb3c1b8ddc27abcb
0biWanKenobi <metal.rulesall@gmail.com>
2021-03-08 22:23:35 +0100

    Application name, id and namespace

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'RRadio Remote',
+      title: 'RMusicPlayer',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
